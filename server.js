d3d3ddf4945bfd0b3dfe10ce6bba56d6ba261cca
Aneerva <73393849+Aneerva@users.noreply.github.com>
2023-06-08 19:29:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,32 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerServer);
-
+var nodeMailler = require('nodemailler')
+const transportrer = nodeMailler.createTransport({
+    port:587,
+    host:'smtp.gmail.com',
+    auth:{
+        user:'aneervajaiswal07@gmail.com',
+        pass:'besspffdnnbkktjz',
+    },
+    secure:true
+})
+app.post('/send-mail',(req,res)=>{
+    const to = req.body.to
+    const url = req.body.url
+    const mailData = {
+        from:'aneervajaiswal07@gmail.com',
+        to: to,
+        subject:'Join The Video Chat With Me :)',
+        html:`<p>Hey There,</p><p>Come & Join Me For A Video Chat Here!! ${url}</p>`
+    }
+transportrer.sendMail(mailData,(error,info)=>{
+    if(error){
+        return console.log(error)
+    }
+    res.status(200).send({message:'invitationSend',message_id:info.messageId})
+})
+})
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
 });
@@ -30,10 +55,11 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
     });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
